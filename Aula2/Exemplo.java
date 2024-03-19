d1207d56602ed46d880e9c1f80d1273f00adec09
Aluno <aluno@facens.br>
2024-03-18 21:42:40 -0300

    my second commit

---
+++ Aula2/Exemplo.java
@@ -0,0 +1,17 @@
+package Aula2;
+ 
+public class Exemplo {
+
+    public static void main(String[]args){
+
+        System.out.println("hellow world");
+        
+
+        
+    }
+
+    
+
+    
+
+}
